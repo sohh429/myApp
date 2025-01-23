dd8866d0a0a21fd8534aba8b45ffb93f17ad9f0f
sohh429 <sohyun0063@gmail.com>
2025-01-24 00:37:31 +0900

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by roxie.h on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myAppTests/myAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  myAppTests.swift
+//  myAppTests
+//
+//  Created by roxie.h on 1/24/25.
+//
+
+import Testing
+
+struct myAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
